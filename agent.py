7904c17751e41429b7ac76e3895535736293638b
Juan Ibarra <jmibarra86@gmail.com>
2023-03-06 17:53:11 -0300

    Terminamos de armar el modelo de entrenamiento

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import deque
 from snake_pygame import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
