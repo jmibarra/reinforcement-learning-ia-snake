0177c39f102cb579eae420d7744ce4382d4a394b
Juan Ibarra <jmibarra86@gmail.com>
2023-03-06 15:51:16 -0300

    Genero la función train con toda su estructura

--- agent.py
+++ agent.py
@@ -12,7 +12,12 @@ LR = 0.001
 class Agent:
 
     def __init__(self):
-        pass
+        self.n_games = 0
+        self.epsilon = 0  # Para controlar la eleatoriedad
+        self.gamma = 0  # Tasa de descuento
+        # Si superamos la memoria removemos elementos de la izquierda "popleft()"
+        self.memory = deque(maxlen=MAX_MEMORY)
+        # TODO: model, trainer
 
     def get_state(self, game):
         pass
@@ -23,7 +28,7 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
 
     def get_action(self, state):
@@ -31,7 +36,41 @@ class Agent:
 
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # obtengo el antiguo estado
+        state_old = agent.get_state(game)
+
+        # obtengo el movimiento
+        final_move = agent.get_action(state_old)
+
+        # Muevo y obtengo el nuevo estado
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # Entreno la memoria corta, 1 paso.
+        agent.train_short_memory(
+            state_old, final_move, reward, state_new, done)
+
+        # Remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # Entreno memoria larga, experiencia
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                # TODO: agent.mode.save()
+
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
 
 if __name__ == '__main__':
